ba89b1d157c826acecf1d01141c0df429eb2466b
Manu <manuelacox9@gmail.com>
2019-03-25 20:07:53 +0000

    x
    x

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def sign_up():
         # A module to make HTTP requests
 
     requests.post(
-            "https://api.mailgun.net/v3/sandbox5af5766a588f49e1bc50d2eb8c5ec041.mailgun.org/messages",
-            auth=("api", "b90ab23a13c88e8ab3543db5fb33b861-acb0b40c-b198b13f"),
-            data={"from": "Excited User <mailgun@sandbox5af5766a588f49e1bc50d2eb8c5ec041.mailgun.org>",
+            "https://api.mailgun.net/v3/sandbox65f5fe854450466e9aa93f4422842a2c.mailgun.org",
+            auth=("api", "13f8d365721a270e4795a3f0446dd867-acb0b40c-e16d050a"),
+            data={"from": "Excited User <mailgun@sandbox65f5fe854450466e9aa93f4422842a2c.mailgun.org>",
                   "to": form_data["email"],
                   "subject": "Hello",
                   "text": form_data["food"]})
