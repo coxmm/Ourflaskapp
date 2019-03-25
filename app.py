bfaca66225f14255cc77b800b5df06f2e9d4fdfd
Manu <manuelacox9@gmail.com>
2019-03-25 20:02:11 +0000

    d
    dd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -17,6 +18,16 @@ def sign_up():
     name = form_data["name"]
     email = form_data["email"]
     # TODO: send email with mailgun
+
+        # A module to make HTTP requests
+
+    requests.post(
+            "https://api.mailgun.net/v3/sandbox5af5766a588f49e1bc50d2eb8c5ec041.mailgun.org/messages",
+            auth=("api", "b90ab23a13c88e8ab3543db5fb33b861-acb0b40c-b198b13f"),
+            data={"from": "Excited User <mailgun@sandbox5af5766a588f49e1bc50d2eb8c5ec041.mailgun.org>",
+                  "to": form_data["email"],
+                  "subject": "Hello",
+                  "text": form_data["food"]})
     return render_template ("thankyouinvite.html", food=food, name=name, email=email)
 
-#app.run(debug=True)
+app.run(debug=True)
